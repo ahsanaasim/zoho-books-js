1d694d43b332cfcab8c93b5239da9f529d4365c7
Ahsan <ahsan.aasim@gmail.com>
2023-03-17 10:42:54 +0600

    build folder deleted

--- build/index.js
+++
@@ -1,9 +0,0 @@
-"use strict";
-Object.defineProperty(exports, "__esModule", { value: true });
-exports.Zoho = void 0;
-const config_1 = require("./config/config");
-const Zoho = (clientId, clientSecret) => {
-    config_1.zoho.clientId = clientId;
-    config_1.zoho.clientSecret = clientSecret;
-};
-exports.Zoho = Zoho;
