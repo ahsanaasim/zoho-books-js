d2ef3d5d03bfd6a59b2332ab2509dca8c395ac8b
Ahsan <ahsan.aasim@gmail.com>
2023-03-18 14:49:29 +0600

    contact helper duplication issue works

--- src/contact/contacts.ts
+++ src/contact/contacts.ts
@@ -11,230 +11,221 @@ export class Contacts {
   contactsUrl: string = baseUrl + 'contacts';
 
   public async getOne(token: string, id: number, params: any): Promise<IContactResponse> {
-    const requestData: IRequestData = {
-      token,
-      url: this.contactsUrl + '/' + id,
-      method: AxiosMethods.get,
-      contentType: '',
-      params,
-      body: undefined,
-    };
     return new Promise((resolve, reject) => {
-      AxiosRequest(requestData, resolve, reject);
+      AxiosRequest(
+        {
+          token,
+          url: this.contactsUrl + '/' + id,
+          method: AxiosMethods.get,
+          contentType: '',
+          params,
+          body: undefined,
+        },
+        resolve,
+        reject,
+      );
     });
   }
 
   public async get(token: string, organizationId: string): Promise<IContactsResponse> {
-    const requestData: IRequestData = {
-      token,
-      url: this.contactsUrl,
-      method: AxiosMethods.get,
-      contentType: AxiosContentTypes.urlencoded,
-      params: {
-        organization_id: organizationId,
-      },
-      body: undefined,
-    };
-
     return new Promise((resolve, reject) => {
-      AxiosRequest(requestData, resolve, reject);
+      AxiosRequest(
+        {
+          token,
+          url: this.contactsUrl,
+          method: AxiosMethods.get,
+          contentType: AxiosContentTypes.urlencoded,
+          params: {
+            organization_id: organizationId,
+          },
+          body: undefined,
+        },
+        resolve,
+        reject,
+      );
     });
   }
 
   public async post(token: string, data: any): Promise<IContactResponse> {
-    const requestData: IRequestData = {
-      token,
-      url: this.contactsUrl,
-      method: AxiosMethods.post,
-      contentType: AxiosContentTypes.json,
-      params: undefined,
-      body: data,
-    };
     return new Promise((resolve, reject) => {
-      AxiosRequest(requestData, resolve, reject);
+      AxiosRequest({
+        token,
+        url: this.contactsUrl,
+        method: AxiosMethods.post,
+        contentType: AxiosContentTypes.json,
+        params: undefined,
+        body: data,
+      }, resolve, reject);
     });
   }
 
   public async update(token: string, id: number, params: any, data: any): Promise<IContactResponse> {
-    const requestData: IRequestData = {
-      token,
-      url: this.contactsUrl + '/' + id,
-      method: AxiosMethods.put,
-      contentType: AxiosContentTypes.json,
-      params,
-      body: data,
-    };
     return new Promise((resolve, reject) => {
-      AxiosRequest(requestData, resolve, reject);
+      AxiosRequest({
+        token,
+        url: this.contactsUrl + '/' + id,
+        method: AxiosMethods.put,
+        contentType: AxiosContentTypes.json,
+        params,
+        body: data,
+      }, resolve, reject);
     });
   }
 
   public async delete(token: string, id: number, params: any): Promise<any> {
-    const requestData: IRequestData = {
-      token,
-      url: this.contactsUrl + '/' + id,
-      method: AxiosMethods.delete,
-      contentType: AxiosContentTypes.urlencoded,
-      params,
-      body: undefined,
-    };
     return new Promise((resolve, reject) => {
-      AxiosRequest(requestData, resolve, reject);
+      AxiosRequest({
+        token,
+        url: this.contactsUrl + '/' + id,
+        method: AxiosMethods.delete,
+        contentType: AxiosContentTypes.urlencoded,
+        params,
+        body: undefined,
+      }, resolve, reject);
     });
   }
 
   public async markAsActive(token: string, id: number, params: any): Promise<IGeneralResponse> {
-    const requestData: IRequestData = {
-      token,
-      url: this.contactsUrl + '/' + id + '/active',
-      method: AxiosMethods.post,
-      contentType: AxiosContentTypes.json,
-      params: params,
-      body: undefined,
-    };
     return new Promise((resolve, reject) => {
-      AxiosRequest(requestData, resolve, reject);
+      AxiosRequest({
+        token,
+        url: this.contactsUrl + '/' + id + '/active',
+        method: AxiosMethods.post,
+        contentType: AxiosContentTypes.json,
+        params: params,
+        body: undefined,
+      }, resolve, reject);
     });
   }
 
   public async markAsInactive(token: string, id: number, params: any): Promise<IGeneralResponse> {
-    const requestData: IRequestData = {
-      token,
-      url: this.contactsUrl + '/' + id + '/inactive',
-      method: AxiosMethods.post,
-      contentType: AxiosContentTypes.json,
-      params: params,
-      body: undefined,
-    };
     return new Promise((resolve, reject) => {
-      AxiosRequest(requestData, resolve, reject);
+      AxiosRequest({
+        token,
+        url: this.contactsUrl + '/' + id + '/inactive',
+        method: AxiosMethods.post,
+        contentType: AxiosContentTypes.json,
+        params: params,
+        body: undefined,
+      }, resolve, reject);
     });
   }
 
-  // contact cannot be inactive and contact person must have email address 
+  // contact cannot be inactive and contact person must have email address
   public async enablePortalAccess(token: string, id: number, params: any, body: any): Promise<IGeneralResponse> {
-    const requestData: IRequestData = {
-      token,
-      url: this.contactsUrl + '/' + id + '/portal/enable',
-      method: AxiosMethods.post,
-      contentType: AxiosContentTypes.json,
-      params: params,
-      body: body,
-    };
     return new Promise((resolve, reject) => {
-      AxiosRequest(requestData, resolve, reject);
+      AxiosRequest({
+        token,
+        url: this.contactsUrl + '/' + id + '/portal/enable',
+        method: AxiosMethods.post,
+        contentType: AxiosContentTypes.json,
+        params: params,
+        body: body,
+      }, resolve, reject);
     });
   }
 
   public async enablePaymentReminders(token: string, id: number, params: any): Promise<IGeneralResponse> {
-    const requestData: IRequestData = {
-      token,
-      url: this.contactsUrl + '/' + id + '/paymentreminder/enable',
-      method: AxiosMethods.post,
-      contentType: AxiosContentTypes.json,
-      params: params,
-      body: undefined,
-    };
     return new Promise((resolve, reject) => {
-      AxiosRequest(requestData, resolve, reject);
+      AxiosRequest({
+        token,
+        url: this.contactsUrl + '/' + id + '/paymentreminder/enable',
+        method: AxiosMethods.post,
+        contentType: AxiosContentTypes.json,
+        params: params,
+        body: undefined,
+      }, resolve, reject);
     });
   }
 
   public async disablePaymentReminders(token: string, id: number, params: any): Promise<IGeneralResponse> {
-    const requestData: IRequestData = {
-      token,
-      url: this.contactsUrl + '/' + id + '/paymentreminder/disable',
-      method: AxiosMethods.post,
-      contentType: AxiosContentTypes.json,
-      params: params,
-      body: undefined,
-    };
     return new Promise((resolve, reject) => {
-      AxiosRequest(requestData, resolve, reject);
+      AxiosRequest({
+        token,
+        url: this.contactsUrl + '/' + id + '/paymentreminder/disable',
+        method: AxiosMethods.post,
+        contentType: AxiosContentTypes.json,
+        params: params,
+        body: undefined,
+      }, resolve, reject);
     });
   }
 
   public async emailStatement(token: string, id: number, params: any, body: any): Promise<IGeneralResponse> {
-    const requestData: IRequestData = {
-      token,
-      url: this.contactsUrl + '/' + id + '/statements/email',
-      method: AxiosMethods.post,
-      contentType: AxiosContentTypes.json,
-      params: params,
-      body: body,
-    };
     return new Promise((resolve, reject) => {
-      AxiosRequest(requestData, resolve, reject);
+      AxiosRequest({
+        token,
+        url: this.contactsUrl + '/' + id + '/statements/email',
+        method: AxiosMethods.post,
+        contentType: AxiosContentTypes.json,
+        params: params,
+        body: body,
+      }, resolve, reject);
     });
   }
 
   public async getStatementMailContent(token: string, id: number, params: any): Promise<IEmailStatementResponse> {
-    const requestData: IRequestData = {
-      token,
-      url: this.contactsUrl + '/' + id + '/statements/email',
-      method: AxiosMethods.get,
-      contentType: AxiosContentTypes.json,
-      params: params,
-      body: undefined,
-    };
     return new Promise((resolve, reject) => {
-      AxiosRequest(requestData, resolve, reject);
+      AxiosRequest({
+        token,
+        url: this.contactsUrl + '/' + id + '/statements/email',
+        method: AxiosMethods.get,
+        contentType: AxiosContentTypes.json,
+        params: params,
+        body: undefined,
+      }, resolve, reject);
     });
   }
 
   public async emailContact(token: string, id: number, params: any, body: any): Promise<IGeneralResponse> {
-    const requestData: IRequestData = {
-      token,
-      url: this.contactsUrl + '/' + id + '/email',
-      method: AxiosMethods.post,
-      contentType: AxiosContentTypes.json,
-      params: params,
-      body: body,
-    };
     return new Promise((resolve, reject) => {
-      AxiosRequest(requestData, resolve, reject);
+      AxiosRequest({
+        token,
+        url: this.contactsUrl + '/' + id + '/email',
+        method: AxiosMethods.post,
+        contentType: AxiosContentTypes.json,
+        params: params,
+        body: body,
+      }, resolve, reject);
     });
   }
 
   public async listComments(token: string, id: number, params: any): Promise<IContactCommentResponse> {
-    const requestData: IRequestData = {
-      token,
-      url: this.contactsUrl + '/' + id + '/comments',
-      method: AxiosMethods.get,
-      contentType: AxiosContentTypes.json,
-      params: params,
-      body: undefined,
-    };
     return new Promise((resolve, reject) => {
-      AxiosRequest(requestData, resolve, reject);
+      AxiosRequest({
+        token,
+        url: this.contactsUrl + '/' + id + '/comments',
+        method: AxiosMethods.get,
+        contentType: AxiosContentTypes.json,
+        params: params,
+        body: undefined,
+      }, resolve, reject);
     });
   }
 
   public async addAdditionalAddress(token: string, id: number, params: any, body: any): Promise<IAddressResponse> {
-    const requestData: IRequestData = {
-      token,
-      url: this.contactsUrl + '/' + id + '/address',
-      method: AxiosMethods.post,
-      contentType: AxiosContentTypes.json,
-      params: params,
-      body: body,
-    };
     return new Promise((resolve, reject) => {
-      AxiosRequest(requestData, resolve, reject);
+      AxiosRequest({
+        token,
+        url: this.contactsUrl + '/' + id + '/address',
+        method: AxiosMethods.post,
+        contentType: AxiosContentTypes.json,
+        params: params,
+        body: body,
+      }, resolve, reject);
     });
   }
 
   public async getContactAddresses(token: string, id: number, params: any): Promise<IAddressesResponse> {
-    const requestData: IRequestData = {
-      token,
-      url: this.contactsUrl + '/' + id + '/address',
-      method: AxiosMethods.get,
-      contentType: AxiosContentTypes.json,
-      params: params,
-      body: undefined,
-    };
     return new Promise((resolve, reject) => {
-      AxiosRequest(requestData, resolve, reject);
+      AxiosRequest({
+        token,
+        url: this.contactsUrl + '/' + id + '/address',
+        method: AxiosMethods.get,
+        contentType: AxiosContentTypes.json,
+        params: params,
+        body: undefined,
+      }, resolve, reject);
     });
   }
 
@@ -245,72 +236,67 @@ export class Contacts {
     params: any,
     body: any,
   ): Promise<IAddressResponse> {
-    const requestData: IRequestData = {
-      token,
-      url: this.contactsUrl + '/' + id + '/address/' + addressId,
-      method: AxiosMethods.put,
-      contentType: AxiosContentTypes.json,
-      params: params,
-      body: body,
-    };
     return new Promise((resolve, reject) => {
-      AxiosRequest(requestData, resolve, reject);
+      AxiosRequest({
+        token,
+        url: this.contactsUrl + '/' + id + '/address/' + addressId,
+        method: AxiosMethods.put,
+        contentType: AxiosContentTypes.json,
+        params: params,
+        body: body,
+      }, resolve, reject);
     });
   }
 
   public async deleteAdditionalAddress(token: string, id: number, addressId: string, params: any): Promise<any> {
-    const requestData: IRequestData = {
-      token,
-      url: this.contactsUrl + '/' + id + '/address/' + addressId,
-      method: AxiosMethods.delete,
-      contentType: AxiosContentTypes.urlencoded,
-      params,
-      body: undefined,
-    };
     return new Promise((resolve, reject) => {
-      AxiosRequest(requestData, resolve, reject);
+      AxiosRequest({
+        token,
+        url: this.contactsUrl + '/' + id + '/address/' + addressId,
+        method: AxiosMethods.delete,
+        contentType: AxiosContentTypes.urlencoded,
+        params,
+        body: undefined,
+      }, resolve, reject);
     });
   }
 
   public async listRefunds(token: string, id: number, params: any): Promise<ICreditnoteRefundsResponse> {
-    const requestData: IRequestData = {
-      token,
-      url: this.contactsUrl + '/' + id + '/refunds',
-      method: AxiosMethods.get,
-      contentType: AxiosContentTypes.json,
-      params: params,
-      body: undefined,
-    };
     return new Promise((resolve, reject) => {
-      AxiosRequest(requestData, resolve, reject);
+      AxiosRequest({
+        token,
+        url: this.contactsUrl + '/' + id + '/refunds',
+        method: AxiosMethods.get,
+        contentType: AxiosContentTypes.json,
+        params: params,
+        body: undefined,
+      }, resolve, reject);
     });
   }
 
   public async track1099(token: string, id: number, params: any): Promise<IGeneralResponse> {
-    const requestData: IRequestData = {
-      token,
-      url: this.contactsUrl + '/' + id + '/track1099',
-      method: AxiosMethods.post,
-      contentType: AxiosContentTypes.json,
-      params: params,
-      body: undefined,
-    };
     return new Promise((resolve, reject) => {
-      AxiosRequest(requestData, resolve, reject);
+      AxiosRequest({
+        token,
+        url: this.contactsUrl + '/' + id + '/track1099',
+        method: AxiosMethods.post,
+        contentType: AxiosContentTypes.json,
+        params: params,
+        body: undefined,
+      }, resolve, reject);
     });
   }
 
   public async untrack1099(token: string, id: number, params: any): Promise<IGeneralResponse> {
-    const requestData: IRequestData = {
-      token,
-      url: this.contactsUrl + '/' + id + '/untrack1099',
-      method: AxiosMethods.post,
-      contentType: AxiosContentTypes.json,
-      params: params,
-      body: undefined,
-    };
     return new Promise((resolve, reject) => {
-      AxiosRequest(requestData, resolve, reject);
+      AxiosRequest({
+        token,
+        url: this.contactsUrl + '/' + id + '/untrack1099',
+        method: AxiosMethods.post,
+        contentType: AxiosContentTypes.json,
+        params: params,
+        body: undefined,
+      }, resolve, reject);
     });
   }
 }
