0067a1eddb11fa2e6dd9241aa997bbc6bad1ef51
Ahsan <ahsan.aasim@gmail.com>
2023-03-20 17:12:31 +0600

    accountTypes added under ChartOfAccounts

--- src/chartofaccounts/chartofaccounts.ts
+++ src/chartofaccounts/chartofaccounts.ts
@@ -3,6 +3,25 @@ import { baseUrl } from '../config/config';
 
 export class ChartOfAccounts {
   chartOfAccountsUrl: string = baseUrl + 'chartofaccounts';
+  static accountTypes = {
+    other_asset: 'other_asset',
+    other_current_asset: 'other_current_asset',
+    cash: 'cash',
+    bank: 'bank',
+    fixed_asset: 'fixed_asset',
+    other_current_liability: 'other_current_liability',
+    credit_card: 'credit_card',
+    long_term_liability: 'long_term_liability',
+    other_liability: 'other_liability',
+    equity: 'equity',
+    income: 'income',
+    other_income: 'other_income',
+    expense: 'expense',
+    cost_of_goods_sold: 'cost_of_goods_sold',
+    other_expense: 'other_expense',
+    accounts_receivable: 'accounts_receivable',
+    accounts_payable: 'accounts_payable',
+  };
 
   public async getOne(token: string, id: string, params: any): Promise<any> {
     const requestData: IRequestData = {
